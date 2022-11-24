3a3978f5337892c30feccdc6cf485304a94f0188
LeonardoEzio <LeonardoEzio@163.com>
2022-11-24 21:15:43 +0800

    配置类优化

--- project-reactor/r2dbc/src/main/java/leonardo/ezio/personal/config/R2dbcConfiguration.java
+++ project-reactor/r2dbc/src/main/java/leonardo/ezio/personal/config/R2dbcConfiguration.java
@@ -1,6 +1,8 @@
 package leonardo.ezio.personal.config;
 
 import cn.hutool.core.util.IdUtil;
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
@@ -9,11 +11,11 @@ import leonardo.ezio.personal.domain.Entity;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
-import org.springframework.data.r2dbc.core.R2dbcEntityOperations;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.r2dbc.dialect.MySqlDialect;
 import org.springframework.data.r2dbc.mapping.R2dbcMappingContext;
@@ -48,43 +50,38 @@ public class R2dbcConfiguration {
 
     @Bean
     @Primary
-    public ConnectionFactory connectionFactory() {
+    public ConnectionFactory connectionFactory(R2dbcProperties properties) {
         final ConnectionFactoryOptions connectionFactoryOptions = ConnectionFactoryOptions.parse(r2dbcProperties.getUrl());
-        return ConnectionFactories.get(
+        ConnectionFactory connectionFactory = ConnectionFactories.get(
                 connectionFactoryOptions.mutate()
                         .option(ConnectionFactoryOptions.DRIVER, "mysql")
                         .option(ConnectionFactoryOptions.USER, r2dbcProperties.getUsername())
                         .option(ConnectionFactoryOptions.PASSWORD, r2dbcProperties.getPassword())
                         .option(Option.valueOf("allowLoadLocalInfile"), true)
-                        .build()
-        );
+                        .build());
+
+        R2dbcProperties.Pool pool = properties.getPool();
+        PropertyMapper propertyMapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
+        ConnectionPoolConfiguration.Builder builder = ConnectionPoolConfiguration.builder(connectionFactory);
+        propertyMapper.from(pool.getMaxIdleTime()).to(builder::maxIdleTime);
+        propertyMapper.from(pool.getMaxLifeTime()).to(builder::maxLifeTime);
+        propertyMapper.from(pool.getMaxAcquireTime()).to(builder::maxAcquireTime);
+        propertyMapper.from(pool.getMaxCreateConnectionTime()).to(builder::maxCreateConnectionTime);
+        propertyMapper.from(pool.getInitialSize()).to(builder::initialSize);
+        propertyMapper.from(pool.getMaxSize()).to(builder::maxSize);
+        propertyMapper.from(pool.getValidationQuery()).whenHasText().to(builder::validationQuery);
+        propertyMapper.from(pool.getValidationDepth()).to(builder::validationDepth);
+        return new ConnectionPool(builder.build());
     }
 
 
     @Bean
     @Primary
-    public R2dbcEntityOperations r2dbcEntityTemplate(ConnectionFactory connectionFactory){
+    public R2dbcEntityTemplate r2dbcEntityTemplate(ConnectionFactory connectionFactory){
         DatabaseClient databaseClient = DatabaseClient.create(connectionFactory);
-        return new R2dbcEntityTemplate(databaseClient, MySqlDialect.INSTANCE);
+        R2dbcEntityTemplate r2dbcEntityTemplate = new R2dbcEntityTemplate(databaseClient, MySqlDialect.INSTANCE);
+        return r2dbcEntityTemplate;
     }
-
-
-//    @Bean(destroyMethod = "dispose")
-//    @Primary
-//    public ConnectionPool connectionPool(R2dbcProperties properties,ConnectionFactory connectionFactory){
-//        R2dbcProperties.Pool pool = properties.getPool();
-//        PropertyMapper propertyMapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
-//        ConnectionPoolConfiguration.Builder builder = ConnectionPoolConfiguration.builder(connectionFactory);
-//        propertyMapper.from(pool.getMaxIdleTime()).to(builder::maxIdleTime);
-//        propertyMapper.from(pool.getMaxLifeTime()).to(builder::maxLifeTime);
-//        propertyMapper.from(pool.getMaxAcquireTime()).to(builder::maxAcquireTime);
-//        propertyMapper.from(pool.getMaxCreateConnectionTime()).to(builder::maxCreateConnectionTime);
-//        propertyMapper.from(pool.getInitialSize()).to(builder::initialSize);
-//        propertyMapper.from(pool.getMaxSize()).to(builder::maxSize);
-//        propertyMapper.from(pool.getValidationQuery()).whenHasText().to(builder::validationQuery);
-//        propertyMapper.from(pool.getValidationDepth()).to(builder::validationDepth);
-//        return new ConnectionPool(builder.build());
-//    }
 
 
     /**
